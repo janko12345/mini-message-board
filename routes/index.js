3e3b091c96e47eae33ffbed82268157cd4253af9
janko12 <henrich.sury97@gmail.com>
2020-09-17 15:03:23 +0200

    everything works perfectly

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages });
+});
+router.get('/new', function (req, res, next) {
+  res.render("form", { title: "Create Message" })
+});
+router.post("/new", (req, res, next) => {
+  let { author, message } = req.body;
+  messages.push({ text: message, user: author, added: new Date() })
+  res.redirect("/");
+});
+
+
+module.exports = router;
